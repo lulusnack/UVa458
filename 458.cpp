76833055c2776712e428ee84ce8a5c459129a8c0
lulusnack <45396092+lulusnack@users.noreply.github.com>
2019-12-13 00:13:28 +0800

    Add files via upload

---
+++ 458.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	string input;
+	while (getline(cin, input))
+	{
+		int len = input.length(); // 取得字串的長度，並避免重複呼叫。
+		for (int i = 0; i < len; i++) // 輸出
+		{
+			if (input[i] == ' ') // 空白不需要解密!
+			{
+				cout << input[i];
+			}
+			else
+				cout << (char)((int)input[i] - 7); // ASCII Code
+		}
+		cout << endl;
+	}
+	return 0;
+} // 結束
